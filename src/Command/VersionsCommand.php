78c8c759209122876fe2faf83f9a998e84b39894
Julien VITTE <j.vitte@alienor.net>
2017-01-05 13:40:45 +0100

    [FEATURE] command to list semver version tags from a git repository

--- src/Command/VersionsCommand.php
+++ src/Command/VersionsCommand.php
@@ -2,7 +2,13 @@
 
 namespace Pitchart\Releazr\Command;
 
+use Composer\Semver\Comparator;
+use Composer\Semver\Semver;
+use Gitonomy\Git\Reference;
 use Gitonomy\Git\Repository;
+use Pitchart\Version\Version;
+use Pitchart\Version\VersionException;
+use Pitchart\Collection\Collection;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputArgument;
@@ -12,15 +18,16 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
-class ListCommand extends Command implements ContainerAwareInterface
+class VersionsCommand extends Command implements ContainerAwareInterface
 {
     use ContainerAwareTrait;
 
     protected function configure()
     {
-        $this->setName('releases')
-            ->setDescription('Lists releases from a github project')
-            ->addOption('path', null, InputOption::VALUE_REQUIRED, 'Path to the local git repository')
+        $this->setName('versions')
+            ->setDescription('Lists version tags from a github project')
+            ->addArgument('path', null, InputArgument::OPTIONAL, 'Path to the local git repository', getcwd())
+            ->addOption('last', null, InputOption::VALUE_NONE, 'Only display the latest version')
         ;
     }
 
@@ -30,28 +37,38 @@ class ListCommand extends Command implements ContainerAwareInterface
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $repositoryPath = $input->getOption('path') ? realpath($input->getOption('path')) : getcwd();
-
-        $repositoryPath = realpath($repositoryPath);
-
-        $output->writeln($repositoryPath);
+        $repositoryPath = realpath($input->getArgument('path'));
 
         $repository = new Repository($repositoryPath);
+        $tags = new Collection($repository->getReferences()->getTags());
 
-        $tags = new \Pitchart\Collection\Collection($repository->getReferences()->getTags());
+        /** @var Collection $versions */
+        $versions = $tags->reduce(function ($accumulator, Reference $tag) {
+            if (preg_match('#refs/tags/v?(?P<version>\d.\d.\d.*)#', $tag->getFullname(), $matches)) {
+                try {
+                    $version = new Version($matches['version']);
+                    $accumulator[] = $version;
+                }
+                catch (VersionException $e) {
+                }
+            }
+            return $accumulator;
+        }, new Collection);
 
-        $versions = $tags->filter(function ($tag) {
-            return preg_match('#refs/tags/v?\d.\d.\d.*#', $tag->getFullname());
-        });
+        if ($input->getOption('last')) {
+            $version = $versions->reduce(function ($accumulator, Version $version) {
+                if (Comparator::greaterThanOrEqualTo($version, $accumulator)) {
+                    $accumulator = $version;
+                }
+                return $accumulator;
+            }, null);
+            $output->writeln((string) $version);
+        }
+        else {
+            $versions->each(function (Version $version) use ($output) {
+                $output->writeln($version->__toString());
+            });
+        }
 
-        $tags->each(function ($version) use ($output) {
-            $output->writeln($version->getFullname());
-        });
-
-        $versions->each(function ($version) use ($output) {
-            $output->writeln($version->getFullname());
-        });
-
-        //var_dump($repository->getReferences()->getTags());
     }
 }
